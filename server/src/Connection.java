cf0e35fba3e38ea72bccf535028a83081eb357c9
Olivier Tremblay-Noel <oliviertremblaynoel@posteo.net>
2023-01-19 10:23:56 -0500

    ameliorer connection

--- server/src/Connection.java
+++ server/src/Connection.java
@@ -1,42 +1,59 @@
+import java.lang.foreign.Addressable;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 class Connection {
-  public Connection() {
-      
-    String adresse_ip;
-    String port;
-    Scanner entreeUtil = new Scanner(System.in);
+    public String adresse_ip = "testip";    
+    public String port = "testport";
 
-    // Addresse IP
-    boolean ipValide = false;
-    boolean portValide = false;
-    while(!ipValide){
-        System.out.println("Entrer l'addresse IP");
+    public void Connection() {
+        //   boolean entreeValide = false;
+    Scanner entreeUtil = new Scanner(System.in);
+      
+    //   String ipRegex = "^(([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])\\.){3}([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5]){1}$";
+    //   // Le regex ci-haut est tiré de :
+    //   // https://stackoverflow.com/questions/31684083/validate-if-input-string-is-a-number-between-0-255-using-regex
+    //   String portRegex = "^(50[0-4][0-9]|5050)$";
+      
+      
+      String messageEntreeIp = "Entrez l'addresse IP";
+    //   String messageEntreePort = "Entrez le port";
+      
+    //   String adresse_ip = "";
+    //   String port = "";
+    // while(!entreeValide){
+        // System.out.println();
         adresse_ip = entreeUtil.nextLine();
-        // TODO demander Bilal si 001.001.001.001 est valide
-        Pattern p = Pattern.compile("^(([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])\\.){3}([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5]){1}$");
-        // Le regex ci-haut est tiré de : https://stackoverflow.com/questions/31684083/validate-if-input-string-is-a-number-between-0-255-using-regex
-        Matcher m = p.matcher(adresse_ip);
-        ipValide = m.matches();
-        if (!ipValide){
-            recommencerEssai();
-        }
-    }
-    while(!portValide){
-        System.out.println("Entrer le Port");
-        port = entreeUtil.nextLine();    
-        Pattern p = Pattern.compile("^(50[0-4][0-9]|5050)$");
-        Matcher m = p.matcher(port);
-        portValide = m.matches();
-        if (!portValide){
-            recommencerEssai();
-        }
-    }
-  }
-  private void recommencerEssai () {
-      System.out.println("Entrée invalide.");
+        adresse_ip = entreeUtil.nextLine();
+        adresse_ip = entreeUtil.nextLine();
+    //     // TODO demander Bilal si 001.001.001.001 est valide
+    //     Pattern p = Pattern.compile("^(([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])\\.){3}([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5]){1}$");
+    //     // Le regex ci-haut est tiré de : https://stackoverflow.com/questions/31684083/validate-if-input-string-is-a-number-between-0-255-using-regex
+    //     Matcher m = p.matcher(adresse_ip);
+    //     ipValide = m.matches();
+    //     if (!ipValide){
+    //         recommencerEssai();
+    //     }
+    // }
+    // entreeValide = false;
+    // while(!entreeValide){
+    //     System.out.println("Entrer le Port");
+    //     port = entreeUtil.nextLine();    
+    //     Pattern p = Pattern.compile("^(50[0-4][0-9]|5050)$");
+    //     Matcher m = p.matcher(port);
+    //     portValide = m.matches();
+    //     if (!portValide){
+    //         recommencerEssai();
+    //     }
+    // }
+}
+// private String match (String message, String regex) {
+
+// }
+
+private void recommencerEssai () {
+    System.out.println("Entrée invalide.");
       boolean essai = true;
       while (essai) {
           System.out.println("Souhaitez-vous réessayer o/n?");
@@ -54,5 +71,8 @@ class Connection {
   }
     public static void main(String[] args){
         Connection a = new Connection();
-      }
-  }
+        // System.out.println(new Connection());
+        System.out.println(a.port);
+        
+    }
+}
